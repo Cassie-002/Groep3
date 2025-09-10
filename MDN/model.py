3f7ccaae5ec01216f17ebee4bff2a867cb2cfefd
Imre Atmodimedjo <i.s.atmodimedjo@student.tue.nl>
2025-09-10 20:07:49 +0200

    Add load model function

--- MDN/model.py
+++ MDN/model.py
@@ -5,7 +5,7 @@ tfb = tfp.bijectors
 tfd = tfp.distributions
 tfpl = tfp.layers
 
-def build_model(nr_gaussians, activation_function, nr_neurons):
+def build_model(nr_gaussians=20, activation_function='relu', nr_neurons=8, learning_rate=1e-4):
     
     event_shape = [2]
     num_components = nr_gaussians
@@ -15,11 +15,17 @@ def build_model(nr_gaussians, activation_function, nr_neurons):
     negloglik = lambda y, p_y: -p_y.log_prob(y)
 
     model = tf.keras.models.Sequential([
-       tf.keras.layers.Dense(nr_neurons, activation=activation_function),
-       tf.keras.layers.Dense(params_size, activation=None),
+       tf.keras.layers.Dense(nr_neurons, activation=activation_function, kernel_initializer='he_normal'),
+       tf.keras.layers.Dense(params_size, activation=None, kernel_initializer='he_normal'),
        tfpl.MixtureSameFamily(num_components, tfpl.IndependentNormal(event_shape)),
     ])
     
-    model.compile(optimizer=tf.optimizers.Adam(learning_rate = 1e-4), loss=negloglik)
+    model.compile(optimizer=tf.optimizers.Adam(learning_rate=learning_rate), loss=negloglik)
 
     return model
+ 
+def load_model(path, x, nr_gaussians=20, activation_function='relu', nr_neurons=8):
+   model = build_model(nr_gaussians, activation_function, nr_neurons)
+   model(x[:1])  # Initialize model
+   model.load_weights(path)
+   return model
