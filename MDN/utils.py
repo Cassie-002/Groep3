1977226089ab5ef5916c33219e3adc3254d5e3ac
Imre Atmodimedjo <i.s.atmodimedjo@student.tue.nl>
2025-09-15 11:49:41 +0200

    Add relative error function

--- MDN/utils.py
+++ MDN/utils.py
@@ -31,10 +31,13 @@ def regline(x, y, intercept=True):
     if not intercept:
         slope = np.sum(x * y) / np.sum(x**2)
         y_hat = slope * x
-        return x, y_hat, slope
+        return x, y_hat, slope, 0
         
     x_mean, y_mean = np.mean(x), np.mean(y)
     slope = np.sum((x - x_mean) * (y - y_mean)) / np.sum((x - x_mean)**2)
     c = y_mean - slope * x_mean
     y_hat = c + slope * x
     return x, y_hat, slope, c
+
+def relative_error(y_true, y_pred):
+    return np.abs((y_true - y_pred) / y_true)
