f520112c1a27e52430739f311d333503841bc2d0
Imre Atmodimedjo <i.s.atmodimedjo@student.tue.nl>
2025-10-10 09:35:51 +0200

    Add disambiguation layers

--- MDN-DSMC/model.py
+++ MDN-DSMC/model.py
@@ -12,6 +12,66 @@ tfb = tfp.bijectors
 tfd = tfp.distributions
 tfpl = tfp.layers
 
+
+class AlphaSigmoid(tf.keras.layers.Layer):
+   def __init__(self, nr_gaussians, param_size):
+      super(AlphaSigmoid, self).__init__()   
+      self.nr_gaussians = nr_gaussians
+      self.param_size = param_size
+      
+      self.mat_alpha, self.mat_mu_sigma = self.create_select_matrix()
+
+   def call(self, x):
+      alpha = tf.linalg.matmul(x, self.mat_alpha)
+      
+      alpha_first = alpha[:, 0]
+      alpha_rest = tf.math.sigmoid(alpha[:, 1:])
+      alpha = tf.concat([alpha_first[:, tf.newaxis], alpha_rest], axis=1)
+      
+      mu_sigma = tf.linalg.matmul(x, self.mat_mu_sigma)
+      return tf.concat([alpha, mu_sigma], axis=1)   
+   
+   def create_select_matrix(self):      
+      mat_alpha = tf.eye(self.nr_gaussians)
+      mat_alpha = tf.pad(mat_alpha, [[0, self.param_size - self.nr_gaussians], [0, 0]])
+
+      mat_mu_sigma = tf.eye(self.param_size-self.nr_gaussians)
+      mat_mu_sigma = tf.pad(mat_mu_sigma, [[self.nr_gaussians, 0], [0, 0]])
+      
+      mat_alpha = tf.cast(mat_alpha, tf.float64)
+      mat_mu_sigma = tf.cast(mat_mu_sigma, tf.float64)
+      
+      return mat_alpha, mat_mu_sigma
+
+
+class Disambiguate(tf.keras.layers.Layer):
+   def __init__(self, nr_gaussians, param_size):
+      super(Disambiguate, self).__init__()     
+      self.nr_gaussians = nr_gaussians
+      self.param_size = param_size
+      
+      self.mat_alpha, self.mat_mu_sigma = self.create_select_matrix()
+   
+   def call(self, x):
+      alpha  = tf.linalg.matmul(x, self.mat_alpha)
+      alpha = tf.math.cumprod(alpha, axis=1)
+      
+      mu_sigma = tf.linalg.matmul(x, self.mat_mu_sigma)
+      return tf.concat([alpha, mu_sigma], axis=1)   
+   
+   def create_select_matrix(self):      
+      mat_alpha = tf.eye(self.nr_gaussians)
+      mat_alpha = tf.pad(mat_alpha, [[0, self.param_size - self.nr_gaussians], [0, 0]])
+
+      mat_mu_sigma = tf.eye(self.param_size-self.nr_gaussians)
+      mat_mu_sigma = tf.pad(mat_mu_sigma, [[self.nr_gaussians, 0], [0, 0]])
+      
+      mat_alpha = tf.cast(mat_alpha, tf.float64)
+      mat_mu_sigma = tf.cast(mat_mu_sigma, tf.float64)
+      
+      return mat_alpha, mat_mu_sigma
+
+
 class Symmetrize(tf.keras.layers.Layer):
    """
    Custom Keras Layer to symmetrize input tensors for Mixture Density Networks (MDN).
@@ -80,11 +140,12 @@ class Symmetrize(tf.keras.layers.Layer):
       return mat_alpha, mat_mu_sigma, mat_mu, mat_sigma
 
 class MDN(tf.keras.Model):
-   def __init__(self, nr_gaussians, nr_neurons, activation_function, symmetrize=False, include_b=False):
+   def __init__(self, nr_gaussians, nr_neurons, activation_function, symmetrize=False, include_b=False, disambiguate=False):
       super(MDN, self).__init__()
       self.nr_gaussians = nr_gaussians
       self.symmetrize = symmetrize
       self.include_b = include_b
+      self.disambiguate = disambiguate
       
       self.params_size = tfpl.MixtureSameFamily.params_size(self.nr_gaussians, component_params_size=tfpl.IndependentNormal.params_size(2))
       
@@ -93,6 +154,10 @@ class MDN(tf.keras.Model):
          
       self.hidden1 = Dense(nr_neurons, activation=activation_function)
       self.hidden2 = Dense(self.params_size, activation=None)
+      
+      if self.disambiguate: 
+         self.alpha_activation = AlphaSigmoid(self.nr_gaussians, self.params_size)
+         self.disambiguate = Disambiguate(self.nr_gaussians, self.params_size)
       
       # Initialize Symmetrize layer after dense layers, for correct use in dsmc.py
       if self.symmetrize:
@@ -104,16 +169,20 @@ class MDN(tf.keras.Model):
       x = self.hidden1(inputs)
       x = self.hidden2(x)
       
+      if self.disambiguate:
+         x = self.alpha_activation(x)
+         x = self.disambiguate(x)
+      
       if self.symmetrize:
          x = self.sym(x, inputs)
       
       return self.mdn(x)
    
 
-def build_model(nr_gaussians=20, activation_function='relu', nr_neurons=8, learning_rate=1e-4, symmetrize=False, include_b=False): 
+def build_model(nr_gaussians=20, activation_function='relu', nr_neurons=8, learning_rate=1e-4, symmetrize=False, include_b=False, disambiguate=False): 
     negloglik = lambda y, p_y: -p_y.log_prob(y)
 
-    model = MDN(nr_gaussians, nr_neurons, activation_function, symmetrize=symmetrize, include_b=include_b)
+    model = MDN(nr_gaussians, nr_neurons, activation_function, symmetrize=symmetrize, include_b=include_b, disambiguate=disambiguate)
     
     model.compile(optimizer=tf.optimizers.Adam(learning_rate=learning_rate), loss=negloglik)
 
@@ -128,7 +197,8 @@ def load_model(model_path, config_path=None):
                        config.get("activation_function"), 
                        config.get("nr_neurons"), 
                        symmetrize=config.get("symmetrize"), 
-                       include_b=config.get("include_b"))
+                       include_b=config.get("include_b"),
+                       disambiguate=config.get("disambiguate"))
    
    # Initialize model
    if config.get("include_b"):
